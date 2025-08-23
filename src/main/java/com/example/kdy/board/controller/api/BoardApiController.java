b3117be4468fb7c66fee3797d384515ba9d5d008
경다영 <yunghkyung95@naver.com>
2025-08-23 23:40:57 +0900

    게시판 API Controller 추가

---
+++ src/main/java/com/example/kdy/board/controller/api/BoardApiController.java
@@ -0,0 +1,25 @@
+package com.example.kdy.board.controller.api;
+
+import com.example.kdy.board.dto.BoardListDTO;
+import com.example.kdy.board.service.BoardService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/board")
+@RequiredArgsConstructor
+public class BoardApiController {
+    private final BoardService boardService; // Board Domain Service
+
+    @GetMapping("/boardList")
+    public List<BoardListDTO> boardList(Model model) { // 게시판 리스트
+        return boardService.boardList();
+    }
+}
