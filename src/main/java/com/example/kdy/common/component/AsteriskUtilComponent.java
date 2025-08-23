94e77851e094c15d8f1d1fb9aaa3a8533d50b2ae
경다영 <yunghkyung95@naver.com>
2025-08-24 03:13:31 +0900

    1. Asterisk : 사용자 이름 적용 2. 리스트에 출력

--- src/main/java/com/example/kdy/common/component/AsteriskUtilComponent.java
+++ src/main/java/com/example/kdy/common/component/AsteriskUtilComponent.java
@@ -9,8 +9,35 @@ import java.util.List;
 @Component
 public class AsteriskUtilComponent {
     public String asterisk_Name_One(String originName) {
-        String tempName = "";
-        return tempName;
+        // 값이 없을 경우
+        if (originName == null || originName.isEmpty()) {
+            return "";
+        }
+
+        // 이름 길이 가져오기
+        int originName_Length = originName.length();
+        
+        // 한 글자일 경우
+        if (originName_Length == 1) {
+            return "정상적인 이름이 아닙니다.";
+
+        // 외자일 경우
+        } else if (originName_Length == 2) {
+            return originName.charAt(0) + "*"; // 성 + 외자 이름
+        
+        // 3글자 이상일 경우
+        } else {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(originName.charAt(0)); // 성
+
+            for (int i = 1; i < originName_Length - 1; i++) {
+                stringBuilder.append("*"); // 이름 사이즈의 -1만큼 Asterisk
+            }
+
+            stringBuilder.append(originName.charAt(originName_Length - 1)); // 마지막 글자
+
+            return stringBuilder.toString();
+        }
     }
 
     public String asterisk_Email_One(String originEmail) {
--- src/main/java/com/example/kdy/user/service/UserService.java
+++ src/main/java/com/example/kdy/user/service/UserService.java
@@ -1,5 +1,10 @@
 package com.example.kdy.user.service;
 
+import com.example.kdy.common.component.AsteriskUtilComponent;
+import com.example.kdy.common.component.DateUtilComponent;
+import com.example.kdy.common.component.IF.AccessorNameInterface;
+
+import com.example.kdy.common.component.ListDateFormatComponent;
 import com.example.kdy.user.dto.UserListDTO;
 import com.example.kdy.user.entity.UserEntity;
 import com.example.kdy.user.mapper.UserMapper;
@@ -12,6 +17,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.function.BiConsumer;
 
 @Slf4j
 @Service
@@ -20,10 +26,41 @@ public class UserService {
     private final UserMapper userMapper;
     private final UserRepository userRepository;
 
+    private final AsteriskUtilComponent asteriskUtilComponent;
+    private final ListDateFormatComponent listDateFormatComponent;
+    private final DateUtilComponent dateUtilComponent;
+
     public List<UserListDTO> userList() { // 사용자 리스트
         Sort sort = Sort.by(Sort.Order.desc("USeq"));
-        List<UserEntity> userList = userRepository.findAll(sort);
 
-        return userMapper.convertToReadListDTO(userList);
+        List<UserEntity> userList = userRepository.findAll(sort);
+        List<UserListDTO> userListDTO = userMapper.convertToReadListDTO(userList);
+
+        if (!userListDTO.isEmpty()) {
+            // 사용자 이름 Asterisk
+            asteriskUtilComponent.asterisk_Name_List(userListDTO, new AccessorNameInterface<UserListDTO>() {
+                @Override
+                public String getAccessName(UserListDTO userListDTO) {
+                    return userListDTO.getUserDTO().getUName();
+                }
+
+                @Override
+                public void setAccessName(UserListDTO userListDTO, String settingName) {
+                    userListDTO.getUserDTO().setUName(settingName);
+                }
+            });
+
+            // 날짜 출력 Format
+            listDateFormatComponent.forListDate(userListDTO, new BiConsumer<UserListDTO, DateUtilComponent>() {
+                @Override
+                public void accept(UserListDTO boardListDTO, DateUtilComponent dateUtilComponent) { // BiConsumer Override
+                    String tempDate = dateUtilComponent.format_String(boardListDTO.getUserDTO().getRegDate(), "yyyyMMdd");
+                    boardListDTO.getUserDTO().setRegDate(tempDate); // 포맷한 데이터 다시 넣기
+                }
+
+            }, dateUtilComponent);
+        }
+
+        return userListDTO;
     }
 }
