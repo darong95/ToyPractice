5078f17f95c67e2e02530c11bef6e529606d61cc
경다영 <yunghkyung95@naver.com>
2025-08-29 20:29:21 +0900

    Entity에 생성자 설정 추가

--- src/main/java/com/example/kdy/user/entity/UserRoleEntity.java
+++ src/main/java/com/example/kdy/user/entity/UserRoleEntity.java
@@ -1,12 +1,17 @@
 package com.example.kdy.user.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Builder
+@Table(name = "TP_USER_ROLE")
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRoleEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
