e2096a89affb196f6bc5c1559be50b22b133c84f
경다영 <yunghkyung95@naver.com>
2025-08-31 14:23:19 +0900

    SPA, Validation 체크

--- src/main/java/com/example/kdy/board/controller/api/BoardApiController.java
+++ src/main/java/com/example/kdy/board/controller/api/BoardApiController.java
@@ -17,12 +17,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -48,15 +46,7 @@ public class BoardApiController {
             boardUpdateDTO.setBoardFileUpdateList(boardFileUpdateList);
         }
 
-        // 에러가 있을 경우 메시지 반환 ➡️ 내용을 보여주기 위함
-        if (bindingResult.hasErrors()) {
-            String validMessage = bindingResult.getAllErrors().stream()
-                    .map(ObjectError::getDefaultMessage)
-                    .collect(Collectors.joining(", "));
-
-            return ResponseEntity.badRequest().body(validMessage); // 400 Bad Request와 함께 에러 메시지 반환
-        }
-
+        // 데이터 처리 (Service)
         boardService.boardUpdate(boardUpdateDTO);
         boardFileService.boardFileDeleteIn(boardUpdateDTO.getBoardFileDeleteList());
         boardFileService.boardFileWrite(boardUpdateDTO.getBoardDTO().getBoardSeq(), boardUpdateDTO.getBoardFileUpdateList());
--- src/main/java/com/example/kdy/common/advice/GlobalExceptionApiAdvice.java
+++ src/main/java/com/example/kdy/common/advice/GlobalExceptionApiAdvice.java
@@ -1,14 +1,17 @@
 package com.example.kdy.common.advice;
 
-import com.example.kdy.auth.controller.api.AuthApiController;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.ObjectError;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice(basePackages = {
@@ -20,5 +23,16 @@ public class GlobalExceptionApiAdvice {
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException illegalArgumentException) {
         log.info("[API ERROR] IllegalArgumentException 발생!");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(illegalArgumentException.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) { // 유효성 검사
+        String validMessage = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(ObjectError::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+
+        return ResponseEntity.badRequest().body(validMessage);
     }
 }
